d26ca327275a8a83e9b8cbf9dbe388c572c6114c
Nam, Nguyen Hai <nguyen.hai.nam@kloon.vn>
2025-07-17 16:35:19 +0700

    test js

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿const params = new URLSearchParams(window.location.search);
+const viewer = params.get("viewer");
+
+if (viewer) {
+    const img = document.getElementById("screenshot");
+    const imgUrl = "https://i.ibb.co/" + viewer;
+    const fallbackImg = "https://dummyimage.com/600x400/333/fff&text=Image+not+found";
+
+    img.src = imgUrl;
+    img.onerror = () => {
+        img.src = fallbackImg;
+        img.alt = "Image not found";
+        document.title = "Ảnh không tồn tại";
+    };
+}
